1b5805b51e1891c05018ccbc09fc1b8e7603d136
tui-la-rua <nam11072k4c36@gmail.com>
2024-10-14 14:31:39 +0700

    ui dang nhap dang ky

--- app/src/main/java/com/example/app_giay/MainActivity.java
+++ app/src/main/java/com/example/app_giay/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app_giay;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.app_giay.view.activities.RegisterActivity;
+import com.example.app_giay.view.activities.SigninActivity;
 
+public class MainActivity extends AppCompatActivity {
+    Button btnSignin, btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnSignin = findViewById(R.id.btnSignin);
+        btnSignin.setOnClickListener(v -> signin());  // Gọi hàm signin khi nhấn nút
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(v -> register());  // Gọi hàm register khi nhấn nút
     }
+
+    public void signin() {
+        // Chỉ thực hiện điều hướng ở đây, không đặt lại OnClickListener
+        Intent intent = new Intent(this, SigninActivity.class);
+        startActivity(intent);
+    }
+
+    public void register() {
+        // Chỉ thực hiện điều hướng ở đây, không đặt lại OnClickListener
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/app_giay/view/activities/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.app_giay.view.activities;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app_giay.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    ImageButton imgBtnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imgBtnBack = findViewById(R.id.imgBtnBack);
+        imgBtnBack.setOnClickListener(v -> finish());
+    }
+}
