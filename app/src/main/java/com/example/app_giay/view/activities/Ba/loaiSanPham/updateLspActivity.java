24ab5db85feda2c182f16bb99a697f65ce8b4e97
tui la rua <125947297+rua-nek@users.noreply.github.com>
2024-10-15 22:34:43 +0700

    update lsp

--- app/src/main/java/com/example/app_giay/view/activities/Ba/loaiSanPham/updateLspActivity.java
+++ app/src/main/java/com/example/app_giay/view/activities/Ba/loaiSanPham/updateLspActivity.java
@@ -2,25 +2,25 @@ package com.example.app_giay.view.activities.Ba.loaiSanPham;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.app_giay.R;
-import com.example.app_giay.dao.loaiSanPhamDao;
 
 public class updateLspActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_lsp);
-
-        // Lấy ID của sản phẩm cần sửa từ Intent
-        int loaiSanPhamId = getIntent().getIntExtra("loaiSanPhamId", -1);
-
-        // Dựa vào loaiSanPhamId để lấy thông tin sản phẩm từ cơ sở dữ liệu hoặc nơi lưu trữ dữ liệu
-        // Hiển thị thông tin sản phẩm cho người dùng có thể sửa
-    }
-    private void updateLoaiSanPham(int lspMa, String newTen, String newMoTa) {
-        loaiSanPhamDao loaiSanPhamDao = new loaiSanPhamDao(this);
-        loaiSanPhamDao.updateLoaiSanPham(lspMa, newTen, newMoTa);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
