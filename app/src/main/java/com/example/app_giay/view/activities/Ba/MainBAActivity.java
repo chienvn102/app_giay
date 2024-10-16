45875e76002322f92df163040d979349996961d9
chienvn102 <chienvn102@gmail.com>
2024-10-16 22:19:23 +0700

    update log out

--- app/src/main/java/com/example/app_giay/view/activities/Ba/MainBAActivity.java
+++ app/src/main/java/com/example/app_giay/view/activities/Ba/MainBAActivity.java
@@ -2,7 +2,9 @@ package com.example.app_giay.view.activities.Ba;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import com.example.app_giay.R;
 import com.example.app_giay.view.activities.Ba.loaiSanPham.lspActivity;
 import com.example.app_giay.view.activities.Ba.nhaSanXuat.nsxActivity;
 import com.example.app_giay.view.activities.Ba.sanPham.sanphamActivity;
+import com.example.app_giay.view.activities.SigninActivity;
 
 public class MainBAActivity extends AppCompatActivity {
     ImageButton imgbtnlsp, imgbtnnsx, imgbtnsp;
@@ -43,5 +46,16 @@ public class MainBAActivity extends AppCompatActivity {
             Intent intent = new Intent(this, sanphamActivity.class);
             startActivity(intent);
         });
+        Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(v -> {
+            // Thực hiện thao tác đăng xuất ở đây
+            Toast.makeText(MainBAActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
+            // Chuyển người dùng về màn hình đăng nhập hoặc kết thúc session
+            Intent intent = new Intent(MainBAActivity.this, SigninActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish(); // Đóng màn hình hiện tại
+        });
+
     }
 }
